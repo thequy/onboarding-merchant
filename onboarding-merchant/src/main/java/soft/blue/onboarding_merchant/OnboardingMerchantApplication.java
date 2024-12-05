c5a55adf1b5164e4d1ed8295e21cfd09bffaf1de
BluEiS <thequy@hotmail.com>
2024-12-05 15:18:21 +0700

    Project Init

---
+++ onboarding-merchant/src/main/java/soft/blue/onboarding_merchant/OnboardingMerchantApplication.java
@@ -0,0 +1,13 @@
+package soft.blue.onboarding_merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingMerchantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingMerchantApplication.class, args);
+	}
+
+}
---
+++ onboarding-merchant/src/test/java/soft/blue/onboarding_merchant/OnboardingMerchantApplicationTests.java
@@ -0,0 +1,13 @@
+package soft.blue.onboarding_merchant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingMerchantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
